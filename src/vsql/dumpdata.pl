d8d04224e6478f8a0087495cdbf93c0bf3adbcc9
Hans-Gert Graebe <graebe@informatik.uni-leipzig.de>
2013-08-25 16:08:14 +0200

    loaddata and dump scripts for Virtuoso isql-vt added

---
+++ src/vsql/dumpdata.pl
@@ -0,0 +1,64 @@
+##################################
+#
+# author: graebe
+# createdAt: 2013-08-25
+# lastModified: 2013-08-25
+
+# purpose: create dumps of Graphs as sorted ntriples via Sparql query.
+# usage: perl dumpdata.pl
+
+# Note that this mechanism is restricted by the size
+
+use strict;
+use SparqlQuery;
+
+saveGraphAsSortedNtriples("People","People.n3");
+saveGraphAsSortedNtriples("Bibliography","BIB-All.n3");
+saveGraphAsSortedNtriples("FreeAlgebras","FreeAlgebras.n3");
+saveGraphAsSortedNtriples("GAlgebras","GAlgebras.n3");
+saveGraphAsSortedNtriples("PolynomialSystems","PolynomialSystems.n3");
+saveGraphAsSortedNtriples("Systems","Systems.n3");
+
+## end main ##
+
+sub getGraph { # get a whole Graph by a construct query
+  my $graphname=shift;
+  my $query=<<EOT;
+construct { ?s ?p ?o } 
+from <http://symbolicdata.org/Data/$graphname/> 
+where {?s ?p ?o}
+EOT
+  my $res=SparqlQuery::query($query);
+  return $res;
+}
+
+sub saveGraphAsSortedNtriples {
+  my ($graph,$target)=@_;
+  open(FH,">/tmp/sdtmp.1");
+  print FH getGraph($graph);
+  close FH;
+  system("rapper -i turtle -o ntriples /tmp/sdtmp.1 | sort > /tmp/sdtmp.2");
+  system("mv /tmp/sdtmp.2 $target");
+  print "Graph $graph saved as $target\n";
+}
+
+__END__
+
+Requires to define this isql-vt function in advance
+
+create procedure graph_to_ttl_file (in graph_iri varchar, in f_name varchar)
+{ declare ses any; 
+  ses := string_output(); 
+  DB.DBA.RDF_GRAPH_TO_TTL (graph_iri, ses); 
+  string_to_file (f_name, ses, 0); 
+}
+
+Note that f_name is owned by the Virtuoso process owner, not the user who
+calls that function.
+
+
+sparql construct { ?s ?p ?o .} from <http://symbolicdata.org/Data/People/> where {?s ?p ?o};
+
+graph_to_ttl_file('http://symbolicdata.org/Data/People/', '/tmp/People.ttl');
+
+
--- src/vsql/loaddata.pl
+++ src/vsql/loaddata.pl
@@ -2,16 +2,19 @@
 #
 # author: graebe
 # createdAt: 2013-08-24
+# lastModified: 2013-08-25
 
 # purpose: load data into the local Virtuoso store
 # usage: perl loaddata.pl | isql-vt 1111 dba <YourSecretPassword>
 
-# all scripts are relative to this environment variable
+# The environment variable SD should point to the root dir of the symbolicdata
+# repo clone. Note that isql-vt does not follow symlinks, hence set 
+#   export SD=/local/home/symbolicdata/... 
+# on symbolicdata.org 
 
-my $SD = $ENV{'SD'}; # root dir of the symbolicdata repo clone
-die "Environment variable SD not set" unless $SD;
+die "Environment variable SD not set" unless $ENV{'SD'};
 
-my $RDFData="$SD/data/RDFData";
+my $RDFData="$ENV{'SD'}/data/RDFData";
 my $out;
 $out.=createLoadCommand("Bibliography","Bibliography.ttl");
 $out.=createLoadCommand("Bibliography","BIB-References.ttl");
@@ -31,3 +34,33 @@ sub createLoadCommand {
 DB.DBA.TTLP_MT (file_to_string_output('$RDFData/$file'),'http://symbolicdata.org/Data/$graph/'); \
 EOT
 }
+
+__END__
+
+
+Some more useful vsql commands:
+
+Clear Data from a given graph:
+sparql clear graph <http://symbolicdata.org/Data/Bibliography/> ; 
+sparql clear graph <http://symbolicdata.org/Data/FanoPolytopes/> ; 
+sparql clear graph <http://symbolicdata.org/Data/FreeAlgebras/> ; 
+sparql clear graph <http://symbolicdata.org/Data/GAlgebras/> ; 
+sparql clear graph <http://symbolicdata.org/Data/GeometryProblems/> ; 
+sparql clear graph <http://symbolicdata.org/Data/People/> ; 
+sparql clear graph <http://symbolicdata.org/Data/PolynomialSystems/> ; 
+sparql clear graph <http://symbolicdata.org/Data/Systems/> ; 
+sparql clear graph <http://symbolicdata.org/Data/TestSets/> ; 
+
+Graphs are not created automatically. If you have problems to display content
+in Ontowiki, such a command may help to resolve the trouble
+
+sparql create silent graph <http://symbolicdata.org/Data/Bibliography/> ; 
+sparql create silent graph <http://symbolicdata.org/Data/FanoPolytopes/> ; 
+sparql create silent graph <http://symbolicdata.org/Data/FreeAlgebras/> ; 
+sparql create silent graph <http://symbolicdata.org/Data/GAlgebras/> ; 
+sparql create silent graph <http://symbolicdata.org/Data/GeometryProblems/> ; 
+sparql create silent graph <http://symbolicdata.org/Data/People/> ; 
+sparql create silent graph <http://symbolicdata.org/Data/PolynomialSystems/> ; 
+sparql create silent graph <http://symbolicdata.org/Data/Systems/> ; 
+sparql create silent graph <http://symbolicdata.org/Data/TestSets/> ; 
+
