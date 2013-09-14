45abd77419823b47283a6797259ab24ec3b7b41c
Hans-Gert Graebe <graebe@informatik.uni-leipzig.de>
2013-09-14 19:03:42 +0200

    Refactoring annotations finished
    References.ttl incorporated into Annotations.ttl

--- src/vsql/loaddata.pl
+++ src/vsql/loaddata.pl
@@ -2,7 +2,7 @@
 #
 # author: graebe
 # createdAt: 2013-08-24
-# lastModified: 2013-09-11
+# lastModified: 2013-09-14
 
 # purpose: load data into the local Virtuoso store
 # usage: perl loaddata.pl | isql-vt 1111 dba <YourSecretPassword>
@@ -17,7 +17,7 @@ die "Environment variable SD not set" unless $ENV{'SD'};
 my $RDFData="$ENV{'SD'}/data/RDFData";
 my $out;
 $out.=createLoadCommand("Bibliography","Bibliography.ttl");
-$out.=createLoadCommand("Bibliography","BIB-References.ttl");
+$out.=createLoadCommand("Annotations","Annotations.ttl");
 $out.=createLoadCommand("FanoPolytopes","FanoPolytopes.ttl");
 $out.=createLoadCommand("FreeAlgebras","FreeAlgebras.ttl");
 $out.=createLoadCommand("GAlgebras","GAlgebras.ttl");
@@ -34,38 +34,10 @@ print $out;
 sub createLoadCommand {
   my ($graph,$file)=@_;
   return<<EOT;
+sparql create silent graph <http://symbolicdata.org/Data/$graph/> ; 
 DB.DBA.TTLP_MT (file_to_string_output('$RDFData/$file'),'http://symbolicdata.org/Data/$graph/'); 
-  sparql create silent graph <http://symbolicdata.org/Data/$graph/> ; 
 EOT
 }
 
 __END__
-
-
-Some more useful vsql commands:
-
-Clear Data from a given graph:
-sparql clear graph <http://symbolicdata.org/Data/Bibliography/> ; 
-sparql clear graph <http://symbolicdata.org/Data/FanoPolytopes/> ; 
-sparql clear graph <http://symbolicdata.org/Data/FreeAlgebras/> ; 
-sparql clear graph <http://symbolicdata.org/Data/GAlgebras/> ; 
-sparql clear graph <http://symbolicdata.org/Data/GeometryProblems/> ; 
-sparql clear graph <http://symbolicdata.org/Data/People/> ; 
-sparql clear graph <http://symbolicdata.org/Data/PolynomialSystems/> ; 
-sparql clear graph <http://symbolicdata.org/Data/Systems/> ; 
-sparql clear graph <http://symbolicdata.org/Data/TestSets/> ; 
-
-Graphs are not created automatically. If you have problems to display content
-in Ontowiki, such a command may help to resolve the trouble.  Now included
-into the above script.
-
-sparql create silent graph <http://symbolicdata.org/Data/Bibliography/> ; 
-sparql create silent graph <http://symbolicdata.org/Data/FanoPolytopes/> ; 
-sparql create silent graph <http://symbolicdata.org/Data/FreeAlgebras/> ; 
-sparql create silent graph <http://symbolicdata.org/Data/GAlgebras/> ; 
-sparql create silent graph <http://symbolicdata.org/Data/GeometryProblems/> ; 
-sparql create silent graph <http://symbolicdata.org/Data/People/> ; 
-sparql create silent graph <http://symbolicdata.org/Data/PolynomialSystems/> ; 
-sparql create silent graph <http://symbolicdata.org/Data/Systems/> ; 
-sparql create silent graph <http://symbolicdata.org/Data/TestSets/> ; 
 
