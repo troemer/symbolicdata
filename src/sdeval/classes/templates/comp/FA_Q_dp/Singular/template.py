e3b09cf54eba98a95ff163632edd5bf78c95cacb
Benjamin Schnitzler <benjaminschnitzler@googlemail.com>
2013-12-19 19:28:55 +0100

    Added markers to Singular template to show where Output is

--- src/sdeval/classes/templates/comp/FA_Q_dp/Singular/template.py
+++ src/sdeval/classes/templates/comp/FA_Q_dp/Singular/template.py
@@ -30,7 +30,9 @@ def generateCode(vars, basis, uptoDeg):
     result += "ideal I = %s;\n" % ",\n".join(FAPolyToSingularStyle(v,vars) for v in basis)
     result += "option(prot);\noption(redTail);\noption(redSB);\n"
     result += "ideal J = letplaceGBasis(I);\n"
+    result += "print (\">>Output Start\");$"
     result += "print (J, \"%s\");$"
+    result += "print (\"<<Output End\");$"
     return result
 
 #--------------------------------------------------
